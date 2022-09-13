30019c4e11b96518442d3fcbf450de74f95767f3
Enrique Custodio <enriquecustodio@Air-de-Enrique.lan>
2022-09-13 09:57:37 -0500

    [feat] Adding Detail Screen

--- shop_app/lib/router/app_routes.dart
+++ shop_app/lib/router/app_routes.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shop_app/screens/screens.dart';
 
 class AppRoutes {
-  static const initialRoute = 'home';
+  static const initialRoute = 'detail';
 
   static Map<String, Widget Function(BuildContext)> getAppRoutes() {
 
     Map<String, Widget Function(BuildContext)> appRoutes = {};
     appRoutes.addAll({'cart' : (BuildContext context) => CartScreen()});
     appRoutes.addAll({'home' : (BuildContext context) => HomeScreen()});
+    appRoutes.addAll({'detail' : (BuildContext context) => DetailScreen()});
 
     // for (final option in menuOptions) {
     //   appRoutes.addAll({option.route : (BuildContext context) => option.screen});
